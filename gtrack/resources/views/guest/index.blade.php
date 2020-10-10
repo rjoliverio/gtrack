dc31a574c8b1977375289fdcc8a49c6df284b7f6
Cloyd Anis <cloyd.anis@gmail.com>
2020-10-10 20:45:11 +0800

    Minor fix

--- gtrack/resources/views/guest/index.blade.php
+++ gtrack/resources/views/guest/index.blade.php
@@ -1,5 +1,4 @@
 @extends('layouts.guest_master')
-
 @section('title')
     GTrack | Home
 @endsection
@@ -8,7 +7,7 @@
         <div class="container text-center my-auto">
         <h1 class="mb-1 app">GTrack</h1>
         <h3 class="mb-5">
-            <em>A Waste Collection Mobile Responsive App</em>
+            <em>A Waste Collection Mobile-Responsive App</em>
         </h3>
         <a class="btn btn-success btn-xl js-scroll-trigger" href="#about">Find Out More</a>
         </div>
@@ -19,7 +18,7 @@
         <div class="row">
             <div class="col-lg-10 mx-auto">
               <h2>GTrack is the solution to your waste collection!</h2>
-              <p class="lead mb-5">The GTrack is a web-based system used by residents and staffs of waste management of a specific barangay in the municipality of Compostela. It aims to manage and provide services for informative and systematic waste collection system with a time-based schedule and route tracking system of the garbage collection. It also includes announcements and report forms specific to issues encountered by garbage collectors as well as from concerned citizens. </p>
+              <p class="lead mb-5" align="justify">The GTrack is a web-based system used by residents and staffs of waste management of a specific barangay in the municipality of Compostela. It aims to manage and provide services for informative and systematic waste collection system with a time-based schedule and route tracking system of the garbage collection. It also includes announcements and report forms specific to issues encountered by garbage collectors as well as from concerned citizens. </p>
               <a class="btn btn-dark btn-xl js-scroll-trigger" href="#services">What We Offer</a>
             </div>
          </div>
@@ -86,7 +85,7 @@
                   <i class="mb-0">Developer</i>
                 </div>
               </div>
-              <img class="img-fluid" src="img/portfolio-4.jpg" alt="">
+              <img class="img-fluid" src="/storage/images/img/humera.jpg" alt="">
             </a>
           </div>
           <div class="col-lg-6">
@@ -124,103 +123,8 @@
           </div>
         </div>
         </div>
-
-
-
-
-
-
       </div>
-
-
-
-
 @endsection
-
-
-@section('team')
-
-@endsection
-
-@section('contact')
-<div >
-  <h1><b>Contact Us</b></h1>
-  <hr>
-    <center>
-      <p class="lead">Feel free to visit (from the given location below) or message us down below for any of your concerns or questions.</p>
-    </center>
-    <br>
-</div>
-<div class="row rowcont ">
-    <div class="col-lg-6 ">
-      <div class="card w-100 contacts" id="contacts2">
-        
-                <div class="info">
-                <h3 class="label h31"><i class="icofont-google-map"></i>&nbsp;Location:</h3>
-                <p>Talamban, Cebu City</p>
-                      
-                
-                <h3 class="label"><i class="icofont-envelope"></i>&nbsp;Email:</h3>
-                <p>OBikes@gmail.com</p>
-                
-                
-                <h3 class="label"><i class="icofont-phone"></i>&nbsp;Call:</h3>
-                <p>+1 5589 55488 555</p>
-                <div class="row maprow">
-                <iframe src="https://www.google.com/maps/d/embed?mid=1yFQDBNLFVyMuO1yrPJCWnnixVA9HmCGg" class="contact-map"></iframe>
-                </div>
-                </div>
-                                  
-      </div>
-    </div>
-<div class="col-lg-6 ">
-  <div class="card w-100 contacts" id="contacts3">
-      <h3 class="label">Send Us A Message</h3>
-      @if(count($errors)>0)
-      <div class="alert alert-danger">
-          <button type="button" class="close" data-dismiss="alert">x</button>
-          <ul>
-              @foreach($errors->all() as $error)
-              <li>{{$error}}</li>
-              @endforeach
-          </ul>
-      </div>
-      @endif
-
-      @if($message = Session::get('success'))
-      <div class="alert alert-success alert-block">
-          <button type="button" class="close" data-dismiss="alert">x</button>
-          <strong>{{$message}}</strong>
-      </div>
-      @endif
-      <form method="post" action="{{ url('/send') }}">
-          {{csrf_field()}}
-          <div class="container w-100">
-              <h4 class="title"></h4>
-
-              @if(isset(Auth::user()->email))
-              <input type="text" name="email" class="form-control form-control-lg w-100 ml-auto mr-auto mb-1"
-                  id="email" placeholder="Email Address" value="{{Auth::user()->email}}" readonly>
-              @else
-              <input type="text" name="email" class="form-control form-control-lg w-100 ml-auto mr-auto mb-1"
-                  id="email" placeholder="Email Address">
-              @endif
-              <input type="text" name="subject" class="form-control form-control-lg w-100 ml-auto mr-auto mb-1"
-                  id="subject" placeholder="Subject">
-              <textarea name="message" class="form-control form-control-lg w-100 ml-auto mr-auto mb-3" name="" id=""
-                  cols="30" rows="10" placeholder="Type your message here...">
-                                  </textarea>
-
-          </div>
-          <div class="sendcont mr-3 mt-1 mb-1 ">
-              <input type="submit" name="send" class="form-control form-control-md btn" id="sendbtn" value="Send">
-          </div>
-      </form>
-  </div>
-</div>
-</div>                   
-@endsection
-
 
 
 
