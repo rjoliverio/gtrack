ae53c8a6bed1b90c363c61facb01ffbcec8fcf80
Humera Ardiente <humera.ardiente@gmail.com>
2020-10-30 16:10:06 +0800

    Added static Calendar

--- gtrack/app/Models/Schedule.php
+++ gtrack/app/Models/Schedule.php
@@ -2,10 +2,27 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+use App\Models\Truck;
 
 class Schedule extends Model
 {
-    use HasFactory;
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $primaryKey='schedule_id';
+    protected $fillable = [
+        'schedule_id','admin_id','schedule','assignment_id','garbage_type','created_at','updated_at'
+    ];
+    public function user()
+    {
+        return $this->hasOne(User::class,'admin_id','user_id');
+    }
+    public function truck()
+    {
+        return $this->hasOne(Truck::class,'assignment_id','truck_id');
+    }
 }
