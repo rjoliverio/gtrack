66fbc2ff58803ac0bb92517d77300b3404616c8d
Rj Oliverio <rjoliverio18@gmail.com>
2020-11-23 17:45:02 +0800

    pdf something error

--- gtrack/vendor/composer/autoload_namespaces.php
+++ gtrack/vendor/composer/autoload_namespaces.php
@@ -6,7 +6,6 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    'Sabberworm\\CSS' => array($vendorDir . '/sabberworm/php-css-parser/lib'),
     'Rize\\UriTemplate' => array($vendorDir . '/rize/uri-template/src'),
     'Mockery' => array($vendorDir . '/mockery/mockery/library'),
     'Google_Service_' => array($vendorDir . '/google/apiclient-services/src'),
